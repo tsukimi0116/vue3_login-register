fd920d10e975419a984db38da8e9b223e730e669
何侑庭(nathanho) <yuting870116@gmail.com>
2024-02-25 22:59:11 +0800

    vite config add base option

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://tsukimi0116.github.io/vue3_login-register/',
   plugins: [
     vue(),
     AutoImport({
